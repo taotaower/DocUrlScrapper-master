0e6edb9775fffe183e299a5b8503492eb5c9123d
Zikun Lin <lin.zik@husky.neu.edu>
2017-08-12 18:07:11 -0400

    delete useless data

--- getChangeConUrls.py
+++ getChangeConUrls.py
@@ -5,69 +5,132 @@ from bs4 import BeautifulSoup
 import urllib
 
 
+"""
+The way to use this file:
 
-files = []
+For get all the urls of document that includes party name 'choiceOfLaw'
 
-f = open('moreChoiceOfLaw.txt')
+1:
+  Run function 'outPutUrls' the input argument is 'https://www.lawinsider.com/clause/choice-of-law'
 
-while 1:
-    file = f.readline()
-    if file:
-        lists = file.split(' ')
+  in this function, you need to change two name of the output files
 
-        lists = filter(lambda x: x != '\n', lists)
-        lists = map(lambda x: x.replace('\n', ''), lists)
-        #num += 1
-        files.append(lists[0])
+------- 'morechoiceOfLaw.txt' store the url that will direct to aimed urls ----
+        f = open("moreChoiceOfLaw.txt", "a")
+        for url in urls:
+            link = url + '\n'
+            f.write(link)
+        f.close()
 
-    else:
-        break
-f.close()
-
-
-contracts = []
-f = open('COL.txt')
-
-while 1:
-    file = f.readline()
-    if file:
-        lists = file.split(' ')
-
-        lists = filter(lambda x: x != '\n', lists)
-        lists = map(lambda x: x.replace('\n', ''), lists)
-        #num += 1
-        contracts.append(lists[0])
-
-    else:
-        break
-f.close()
+------- 'COL.txt' aimed urls ----
 
 
+        f = open("COL.txt","a")
 
-num= 0
-for file in files:
-    htmlBytes = urllib.urlopen(file).read()
-    html = htmlBytes.decode("utf8")
-    soup = BeautifulSoup(html, 'lxml')
+        for c in contracts:
+            contract = c + '\n'
+            f.write(contract)
+        f.close()
 
-    links = soup.find_all('a',href=re.compile(r"^/contracts/"))
-    discard = soup.find_all('a',href=re.compile(r"^/contracts/tagged/"))
-    for link in discard:
-        link.extract()
-    f = open("COL.txt", "a")
-    for link in links:
-        num +=1
-        print num
-        l = link.get('href')
-        if l in contracts:
-            print 'already in here'
+2.
+        Run function 'getAllUrls()'
+
+        you also need to change the file name of the file that the function will open,
+
+        and the result file's name
+
+        those two files' name you need to change are same as you use in function 'outPutUrls'
+
+
+
+
+------- 'morechoiceOfLaw.txt' store the url that will direct to aimed urls ----
+
+    files = []
+
+    f = open('moreChoiceOfLaw.txt')
+
+    while 1:
+        file = f.readline()
+        if file:
+
+------- 'COL.txt' aimed urls   the output result we need----
+
+    contracts = []
+    f = open('COL.txt')
+
+    while 1:
+        file = f.readline()
+        if file:
+            lists = file.split(' ')
+
+
+s
+"""
+
+
+
+def getAllUrls():
+
+    files = []
+
+    f = open('moreChoiceOfLaw.txt')
+
+    while 1:
+        file = f.readline()
+        if file:
+            lists = file.split(' ')
+
+            lists = filter(lambda x: x != '\n', lists)
+            lists = map(lambda x: x.replace('\n', ''), lists)
+            #num += 1
+            files.append(lists[0])
+
         else:
-            l = l + '\n'
-            f.write(l)
+            break
+    f.close()
+
+
+    contracts = []
+    f = open('COL.txt')
+
+    while 1:
+        file = f.readline()
+        if file:
+            lists = file.split(' ')
+
+            lists = filter(lambda x: x != '\n', lists)
+            lists = map(lambda x: x.replace('\n', ''), lists)
+            #num += 1
+            contracts.append(lists[0])
+
+        else:
+            break
     f.close()
 
 
 
+    num= 0
+    for file in files:
+        htmlBytes = urllib.urlopen(file).read()
+        html = htmlBytes.decode("utf8")
+        soup = BeautifulSoup(html, 'lxml')
+
+        links = soup.find_all('a',href=re.compile(r"^/contracts/"))
+        discard = soup.find_all('a',href=re.compile(r"^/contracts/tagged/"))
+        for link in discard:
+            link.extract()
+        f = open("COL.txt", "a")
+        for link in links:
+            num +=1
+            print num
+            l = link.get('href')
+            if l in contracts:
+                print 'already in here'
+            else:
+                l = l + '\n'
+                f.write(l)
+        f.close()
 
 
 
@@ -127,8 +190,6 @@ def outPutUrls(url):
             f.write(contract)
         f.close()
 
-outPutUrls('https://www.lawinsider.com/clause/choice-of-law')
+#outPutUrls('https://www.lawinsider.com/clause/choice-of-law')
 
-'''
-
-'''
+getAllUrls()
